63cc2667e17ab494b60ce7f2fa2520dd79cf4b09
vivisekught <nikitamaliy09@gmail.com>
2023-04-03 15:44:16 +0300

    Create layout for player

---
+++ app/src/main/java/com/example/mediaplayerapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mediaplayerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+    }
+}
